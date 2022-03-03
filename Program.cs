b145f0cd686282d236ace70ca58115da13525ece
Ieva Petrusenkova <ieva.pilenge@gmail.com>
2022-03-03 20:36:13 +0200

    Ieva P.task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+short A = 82;
+short B = 21;
+Console.WriteLine(Math.Max(A, B));
+Console.WriteLine(Math.Min(A, B));
+
+if (A == B)
+{
+    Console.WriteLine("They are equal");
+}
+else
+{
+    Console.WriteLine("They are not equal");
+}
+if (A % 2 == 0) 
+{
+    Console.WriteLine($"The number {A} is even");
+}
+else
+{
+    Console.WriteLine($"The number {A} is odd");
+}
+if (B % 2 == 0)
+{
+    Console.WriteLine($"The number {B} is even");
+}
+else
+{
+    Console.WriteLine($"The number {B} is odd");
+}
+if (A < 0)
+{
+    Console.WriteLine($"The number {A} is negative");
+}
+else
+{
+    Console.WriteLine($"The number {A} is positive");
+}
